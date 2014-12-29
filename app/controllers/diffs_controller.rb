bf8a4d838a4ceab073568f453a6fe44e428d2c1e
Jason Ku <jku@jasontku.com>
2014-12-29 13:54:20 -0800

    Hook up diff form to controller.

--- app/controllers/diffs_controller.rb
+++ app/controllers/diffs_controller.rb
@@ -1,7 +1,10 @@
 class DiffsController < ApplicationController
   def create
+    diff = Diff.new(params[:diff])
 
-    @diff = Diff.new(params[:diff])
-
+    render json: {
+      success: true,
+      results: diff.to_html,
+    }
   end
 end
