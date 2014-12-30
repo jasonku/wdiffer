263510b54244f000b34db9092390c56566b3c967
Jason Ku <jasontku@gmail.com>
2014-12-29 19:21:21 -0800

    Escape HTML input.

--- app/models/diff.rb
+++ app/models/diff.rb
@@ -10,14 +10,22 @@ class Diff
   def to_html(options)
     ignore_case = options[:ignore_case]
 
+    if @expected.present?
+      @expected = CGI::escape_html(@expected)
+    end
+
+    if @actual.present?
+      @actual = CGI::escape_html(@actual)
+    end
+
     if ignore_case
-      expected.downcase!
-      actual.downcase!
+      @expected.downcase!
+      @actual.downcase!
     end
 
     diff =
-      if expected.present? || actual.present?
-        Diff.diff(actual, expected)
+      if @expected.present? || @actual.present?
+        Diff.diff(@actual, @expected)
       end
 
     diff || "No diff."
